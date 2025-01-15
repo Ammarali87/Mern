f79f3658a4c31e5ec7ce4495b90e47910c0cd7fd
Ammarali87 <amarhero501@gmail.com>
2025-01-15 12:13:34 +0200

    rote

--- front/src/pages/HomePage.tsx
+++ front/src/pages/HomePage.tsx
@@ -9,6 +9,7 @@ import { getError } from '../utils';
 
 export default function HomePage() {
   const { data: products, isLoading, error } = useGetProductsQuery();
+  console.log("https://ecommerce.routemisr.com/api/v1/products/6428de2adc1175abc65ca05b");
 
   return (
     <Container>
@@ -17,7 +18,7 @@ export default function HomePage() {
       </Helmet>
       <h2 className="text-center my-4">Welcome to TS Amazona</h2>
       <h5 className="text-center mb-4">Shop your favorite items</h5>
-
+         
       {isLoading ? (
         <LoadingBox />
       ) : error ? (
